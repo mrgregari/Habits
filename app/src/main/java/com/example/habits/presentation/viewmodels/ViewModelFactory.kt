45f874650f24cb400af439b10429cf12245bb9f2
Georgy <gmy-2015@yandex.ru>
2025-01-25 21:26:05 +0300

    Add ViewModelFactory

--- app/src/main/java/com/example/habits/presentation/viewmodels/ViewModelFactory.kt
+++ app/src/main/java/com/example/habits/presentation/viewmodels/ViewModelFactory.kt
@@ -2,10 +2,11 @@ package com.example.habits.presentation.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.habits.di.ApplicationScope
 import javax.inject.Inject
 import javax.inject.Provider
 
-
+@ApplicationScope
 class ViewModelFactory @Inject constructor(
     private val viewModelProviders: @JvmSuppressWildcards Map<Class<out ViewModel>, Provider<ViewModel>>
 ) : ViewModelProvider.Factory {
